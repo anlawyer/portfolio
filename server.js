c79f1f082a5e819ae4c84b4bc118ec82325fac89
Alison Lawyer <alison@Alisons-MacBook-Air.local>
2018-01-16 08:55:27 -0800

    added simple server to deploy site

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+var PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static('public'));
+
+// require('./routes/api-routes.js')(app);
+
+app.listen(PORT, function () {
+  console.log('App listening on PORT: ' + PORT);
+});
